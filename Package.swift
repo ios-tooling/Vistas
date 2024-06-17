207a5aed6eacf20005df6b173b1a1c4e3e6d6792
Ben Gottlieb <ben@standalone.com>
2024-06-17 05:51:11 -0500

    added WebScreen

--- Package.swift
+++ Package.swift
@@ -19,10 +19,11 @@ let package = Package(
     ],
 	 dependencies: [
 		.package(url: "https://github.com/ios-tooling/Suite", from: "1.0.137"),
+		.package(url: "https://github.com/ios-tooling/CrossPlatformKit", from: "1.0.11"),
 	 ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-        .target(name: "Vistas", dependencies: ["Suite"]),
+        .target(name: "Vistas", dependencies: ["Suite", "CrossPlatformKit"]),
     ]
 )
--- Sources/Vista/WebScreen.swift
+++ Sources/Vista/WebScreen.swift
@@ -1,11 +1,83 @@
 //
 //  WebScreen.swift
-//  
+//
 //
 //  Created by Ben Gottlieb on 6/17/24.
 //
 
 import SwiftUI
 import WebKit
+import CrossPlatformKit
 
+public struct WebScreen: UXViewRepresentable {
+	let url: URL?
+	let html: String?
+	var didFinishLoading: ((WKWebView) -> Void)?
+	
+	public init(url: URL, didFinishLoading: ((WKWebView) -> Void)? = nil) {
+		self.url = url
+		self.html = nil
+		self.didFinishLoading = didFinishLoading
+	}
+	
+	public init(html: String, didFinishLoading: ((WKWebView) -> Void)? = nil) {
+		self.html = html
+		self.url = nil
+		self.didFinishLoading = didFinishLoading
+	}
+	
+	public func makeUXView(context: Context) -> WKWebView {
+		context.coordinator.didFinishLoading = didFinishLoading
+		
+		if let url {
+			if url.isFileURL {
+				context.coordinator.webView.loadFileURL(url, allowingReadAccessTo: url.deletingLastPathComponent())
+			} else {
+				context.coordinator.webView.load(URLRequest(url: url))
+			}
+		} else if let html {
+			context.coordinator.webView.loadHTMLString(html, baseURL: nil)
+		}
+		return context.coordinator.webView
+	}
+	
+	public func updateUXView(_ uiView: WKWebView, context: Context) {
+		context.coordinator.didFinishLoading = didFinishLoading
+	}
+	
+	public func makeCoordinator() -> Coordinator {
+		Coordinator(url: url, html: html)
+	}
+	
+	public class Coordinator: NSObject, WKNavigationDelegate {
+		let url: URL?
+		var html: String?
+		var webView: InternalWebView!
+		var didFinishLoading: ((WKWebView) -> Void)?
+		
+		static var cachedWebViews: [URL: InternalWebView] = [:]
+		
+		init(url: URL?, html: String?) {
+			self.url = url
+			self.html = html
+			super.init()
+			
+			if let url, let view = Self.cachedWebViews[url] {
+				webView = view
+			} else {
+				webView = InternalWebView(frame: .zero)
+				if let url { Self.cachedWebViews[url] = webView }
+			}
+			webView.navigationDelegate = self
+		}
+		
+		public func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+			didFinishLoading?(webView)
+		}
+	}
+	
+	class InternalWebView: WKWebView {
+		
+	}
+}
 
