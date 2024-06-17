157d6dd2518d105dc9b5963666ee65982cf9df6e
Ben Gottlieb <ben@standalone.com>
2024-06-17 05:40:27 -0500

    initial package commit

---
+++ Package.swift
@@ -0,0 +1,28 @@
+// swift-tools-version: 6.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+import CompilerPluginSupport
+
+let package = Package(
+    name: "Vistas",
+     platforms: [
+              .macOS(.v12),
+              .iOS(.v16),
+              .watchOS(.v10),
+         ],
+    products: [
+        // Products define the executables and libraries produced by a package, and make them visible to other packages.
+        .library(
+            name: "Vistas",
+            targets: ["Vistas"]),
+    ],
+	 dependencies: [
+		.package(url: "https://github.com/ios-tooling/Suite", from: "1.0.137"),
+	 ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+        .target(name: "Vistas", dependencies: ["Suite"]),
+    ]
+)
---
+++ Sources/Vista/WebScreen.swift
@@ -0,0 +1,11 @@
+//
+//  WebScreen.swift
+//  
+//
+//  Created by Ben Gottlieb on 6/17/24.
+//
+
+import SwiftUI
+import WebKit
+
+
