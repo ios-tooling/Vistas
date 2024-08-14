e32efada2f5e2e47332c764bba1598778fbb37a1
Ben Gottlieb <ben@standalone.com>
2024-08-14 05:36:10 -0500

    added isLoading binding to webView, as well as reload

--- Sources/Vista/WebScreen.swift
+++ Sources/Vista/WebScreen.swift
@@ -12,18 +12,21 @@ import CrossPlatformKit
 public struct WebScreen: UXViewRepresentable {
 	let url: URL?
 	let html: String?
-	var didFinishLoading: ((WKWebView) -> Void)?
+	var didFinishLoading: ((WKWebView, Error?) -> Void)?
+	var isLoading: Binding<Bool>?
 	
-	public init(url: URL, didFinishLoading: ((WKWebView) -> Void)? = nil) {
+	public init(url: URL, isLoading: Binding<Bool>? = nil, didFinishLoading: ((WKWebView, Error?) -> Void)? = nil) {
 		self.url = url
 		self.html = nil
 		self.didFinishLoading = didFinishLoading
+		self.isLoading = isLoading
 	}
 	
-	public init(html: String, didFinishLoading: ((WKWebView) -> Void)? = nil) {
+	public init(html: String, isLoading: Binding<Bool>? = nil, didFinishLoading: ((WKWebView, Error?) -> Void)? = nil) {
 		self.html = html
 		self.url = nil
 		self.didFinishLoading = didFinishLoading
+		self.isLoading = isLoading
 	}
 	
 	public func makeUXView(context: Context) -> WKWebView {
@@ -43,23 +46,29 @@ public struct WebScreen: UXViewRepresentable {
 	
 	public func updateUXView(_ uiView: WKWebView, context: Context) {
 		context.coordinator.didFinishLoading = didFinishLoading
+		if url != context.coordinator.url {
+			context.coordinator.url = url
+		}
 	}
 	
 	public func makeCoordinator() -> Coordinator {
-		Coordinator(url: url, html: html)
+		Coordinator(url: url, html: html, isLoading: isLoading)
 	}
 	
 	public class Coordinator: NSObject, WKNavigationDelegate {
-		let url: URL?
+		var url: URL? { didSet { updateWebView() }}
 		var html: String?
 		var webView: InternalWebView!
-		var didFinishLoading: ((WKWebView) -> Void)?
+		var didFinishLoading: ((WKWebView, Error?) -> Void)?
+		var isLoadingObserver: Any?
+		let isLoading: Binding<Bool>?
 		
 		static var cachedWebViews: [URL: InternalWebView] = [:]
 		
-		init(url: URL?, html: String?) {
+		init(url: URL?, html: String?, isLoading: Binding<Bool>?) {
 			self.url = url
 			self.html = html
+			self.isLoading = isLoading
 			super.init()
 			
 			if let url, let view = Self.cachedWebViews[url] {
@@ -69,10 +78,22 @@ public struct WebScreen: UXViewRepresentable {
 				if let url { Self.cachedWebViews[url] = webView }
 			}
 			webView.navigationDelegate = self
+			isLoadingObserver = webView.observe(\.isLoading) { [weak self] view, change in
+				self?.isLoading?.wrappedValue = view.isLoading
+			}
+		}
+				
+		public func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+			didFinishLoading?(webView, nil)
 		}
 		
-		public func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
-			didFinishLoading?(webView)
+		public func webView(_ webView: WKWebView, didFail navigation: WKNavigation!, withError error: any Error) {
+			didFinishLoading?(webView, error)
+		}
+		
+		func updateWebView() {
+			guard let url else { return }
+			webView.load(.init(url: url))
 		}
 	}
 	
