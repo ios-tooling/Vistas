f41367f6ed3a288ea565912c259be5a1e1ac0775
Ben Gottlieb <ben@standalone.com>
2026-02-08 10:05:27 -0600

    created CameraManager and CameraView stubs

---
+++ Sources/Vista/Camera View/CameraManager.swift
@@ -0,0 +1,19 @@
+//
+//  File.swift
+//  Vistas
+//
+//  Created by Ben Gottlieb on 2/8/26.
+//
+
+import SwiftUI
+
+#if os(iOS)
+
+@available(iOS 17, *)
+@MainActor @Observable public class CameraManager {
+	public static let instance = CameraManager()
+	
+	
+}
+
+#endif
